e993c07a12a5c4a1c7f425360097a22e2fc8c840
namvu <namvu61097@gmail.com>
2023-12-13 10:15:38 +0700

    [Add] Update Post API

--- src/main/java/com/vti/blogapp/service/PostService.java
+++ src/main/java/com/vti/blogapp/service/PostService.java
@@ -2,6 +2,7 @@ package com.vti.blogapp.service;
 
 import com.vti.blogapp.dto.PostDto;
 import com.vti.blogapp.form.PostCreateForm;
+import com.vti.blogapp.form.PostUpdateForm;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -9,4 +10,6 @@ public interface PostService {
     Page<PostDto> findAll(Pageable pageable);
 
     PostDto create(PostCreateForm form);
+
+    PostDto update(PostUpdateForm form, Long id);
 }
--- src/main/java/com/vti/blogapp/service/PostServiceImpl.java
+++ src/main/java/com/vti/blogapp/service/PostServiceImpl.java
@@ -1,16 +1,14 @@
 package com.vti.blogapp.service;
 
 import com.vti.blogapp.dto.PostDto;
-import com.vti.blogapp.entity.Post;
 import com.vti.blogapp.form.PostCreateForm;
+import com.vti.blogapp.form.PostUpdateForm;
 import com.vti.blogapp.mapper.PostMapper;
 import com.vti.blogapp.repository.PostRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 
 @Service
 @AllArgsConstructor
@@ -27,6 +25,14 @@ public class PostServiceImpl implements PostService{
     @Override
     public PostDto create(PostCreateForm form) {
         var post = PostMapper.map(form);
+        var savePost = postRepository.save(post);
+        return PostMapper.map(savePost);
+    }
+
+    @Override
+    public PostDto update(PostUpdateForm form, Long id) {
+        var post = PostMapper.map(form);
+        post.setId(id);
         var savePost = postRepository.save(post);
         return PostMapper.map(savePost);
     }
