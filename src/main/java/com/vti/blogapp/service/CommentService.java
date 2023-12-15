a7f95348fe1ab1fd3c34837946bab7f8fced0e95
namvu <namvu61097@gmail.com>
2023-12-15 10:02:27 +0700

    [Add] Create Comment API

---
+++ src/main/java/com/vti/blogapp/service/CommentService.java
@@ -0,0 +1,9 @@
+package com.vti.blogapp.service;
+
+import com.vti.blogapp.dto.CommentDto;
+import com.vti.blogapp.dto.PostDto;
+import com.vti.blogapp.form.CommentCreateForm;
+
+public interface CommentService {
+    CommentDto create(CommentCreateForm form, Long postId);
+}
