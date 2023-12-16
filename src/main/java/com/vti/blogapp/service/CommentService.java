a795c3d90f9a10b405291252d64e7c90ebc65099
namvu <namvu61097@gmail.com>
2023-12-16 19:48:14 +0700

    [Add] Update & Delete comment API

--- src/main/java/com/vti/blogapp/service/CommentService.java
+++ src/main/java/com/vti/blogapp/service/CommentService.java
@@ -2,10 +2,16 @@ package com.vti.blogapp.service;
 
 import com.vti.blogapp.dto.CommentDto;
 import com.vti.blogapp.form.CommentCreateForm;
+import com.vti.blogapp.form.CommentUpdateForm;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface CommentService {
     Page<CommentDto> findAll(Pageable pageable);
+
     CommentDto create(CommentCreateForm form, Long postId);
+
+    CommentDto update(CommentUpdateForm form, Long id);
+
+    void deleteById(Long id);
 }
