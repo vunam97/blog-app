8202e50dba7d11b2bbbcc802c7ac448e2283a358
namvu <namvu61097@gmail.com>
2023-12-23 09:02:17 +0700

    [Add] Validate postCreateForm

--- src/main/java/com/vti/blogapp/controller/PostController.java
+++ src/main/java/com/vti/blogapp/controller/PostController.java
@@ -5,11 +5,14 @@ import com.vti.blogapp.form.PostCreateForm;
 import com.vti.blogapp.form.PostFilterForm;
 import com.vti.blogapp.form.PostUpdateForm;
 import com.vti.blogapp.service.PostService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Validated
 @RestController
 @AllArgsConstructor
 public class PostController {
@@ -26,7 +29,7 @@ public class PostController {
     }
 
     @PostMapping("/api/v1/posts")
-    public PostDto create(@RequestBody PostCreateForm form) {
+    public PostDto create(@RequestBody @Valid PostCreateForm form) {
         return postService.create(form);
     }
 
