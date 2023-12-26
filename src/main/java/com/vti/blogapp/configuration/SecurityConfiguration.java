7f118090505cf4ba07ce5faa939cc12a73c26d57
namvu <namvu61097@gmail.com>
2023-12-26 19:18:18 +0700

    [Add] Encode user password

--- src/main/java/com/vti/blogapp/configuration/SecurityConfiguration.java
+++ src/main/java/com/vti/blogapp/configuration/SecurityConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,5 +25,10 @@ public class SecurityConfiguration {
                 )
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/vti/blogapp/service/UserServiceImpl.java
+++ src/main/java/com/vti/blogapp/service/UserServiceImpl.java
@@ -5,16 +5,20 @@ import com.vti.blogapp.form.UserCreateForm;
 import com.vti.blogapp.mapper.UserMapper;
 import com.vti.blogapp.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService{
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDto create(UserCreateForm form) {
         var user = UserMapper.map(form);
+        var encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
         var savedUser = userRepository.save(user);
         return UserMapper.map(savedUser);
     }
