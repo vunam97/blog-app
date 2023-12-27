b78e7ba175985ac6000392f5ee59aaa8afb0c5e5
namvu <namvu61097@gmail.com>
2023-12-27 10:16:02 +0700

    [Add] Login using database

--- src/main/java/com/vti/blogapp/service/UserServiceImpl.java
+++ src/main/java/com/vti/blogapp/service/UserServiceImpl.java
@@ -5,12 +5,17 @@ import com.vti.blogapp.form.UserCreateForm;
 import com.vti.blogapp.mapper.UserMapper;
 import com.vti.blogapp.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService, UserDetailsService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
@@ -21,5 +26,18 @@ public class UserServiceImpl implements UserService{
         user.setPassword(encodedPassword);
         var savedUser = userRepository.save(user);
         return UserMapper.map(savedUser);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new User(
+                user.getUsername(),
+                user.getPassword(),
+                AuthorityUtils.NO_AUTHORITIES
+        );
     }
 }
